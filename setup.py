79a728e1bf8783200ea8ce0fa2ba9bdf10ba4e16
GleibsonCardoso <59875267+GleibsonCardoso@users.noreply.github.com>
2022-10-16 19:54:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="slider_grafic",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="gleibson",
+    author_email="gleibson11@gmail.com",
+    description="Cria um grafico com slider para um simulador de biossensor óptico",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/GleibsonCardoso/silder_grafic_projetc",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
